450a3dd49e740b8ccd358846fd0948adedd1d5d5
claudio <camposclaudio@live.com>
2023-04-20 20:37:02 -0300

    sacando vite

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,61 +5,68 @@
                 <div class="card bg-light text-dark">
                     <div class="card-body">
                         <h4 class="card-title">Registrarse</h4>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+                        <form method="POST" action="{{ route('register') }}" class="needs-validation" novalidate>
+                            @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                            <div class="mb-3">
+                                <label for="name" class="form-label">{{ __('Name') }}</label>
+                                <input id="name" class="form-control @error('name') is-invalid @enderror"
+                                    type="text" name="name" value="{{ old('name') }}" required autofocus
+                                    autocomplete="name" />
+                                <div class="invalid-feedback">
+                                    @error('name')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="email" class="form-label">{{ __('Email') }}</label>
+                                <input id="email" class="form-control @error('email') is-invalid @enderror"
+                                    type="email" name="email" value="{{ old('email') }}" required
+                                    autocomplete="username" />
+                                <div class="invalid-feedback">
+                                    @error('email')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="password" class="form-label">{{ __('Password') }}</label>
+                                <input id="password" class="form-control @error('password') is-invalid @enderror"
+                                    type="password" name="password" required autocomplete="new-password" />
+                                <div class="invalid-feedback">
+                                    @error('password')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="password_confirmation"
+                                    class="form-label">{{ __('Confirm Password') }}</label>
+                                <input id="password_confirmation"
+                                    class="form-control @error('password_confirmation') is-invalid @enderror"
+                                    type="password" name="password_confirmation" required autocomplete="new-password" />
+                                <div class="invalid-feedback">
+                                    @error('password_confirmation')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-3 d-flex justify-content-end">
+                                <a href="{{ route('login') }}" class="me-2">{{ __('Already registered?') }}</a>
+                                <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+
+
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</div>
-</div>
-
-
-</div>
-</div>
-</div>
+    </div>
 </x-guest-layout>
